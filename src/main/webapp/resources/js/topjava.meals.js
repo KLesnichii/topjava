b4433eb9037d0edb237f6268fe10ab2aa2d6bcbf
KLesnichii <due9@yandex.ru>
2020-08-20 20:31:45 +0300

    HW08

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,36 @@
+$(function () {
+    makeEditable({
+            ajaxUrl: "ajax/admin/meals/",
+            datatableApi: $("#datatable").DataTable({
+                "paging": false,
+                "info": false,
+                "searching": false,
+                "columns": [
+                    {
+                        "data": "dateTime"
+                    },
+                    {
+                        "data": "description"
+                    },
+                    {
+                        "data": "calories"
+                    },
+                    {
+                        "defaultContent": "Edit",
+                        "orderable": false
+                    },
+                    {
+                        "defaultContent": "Delete",
+                        "orderable": false
+                    }
+                ],
+                "order": [
+                    [
+                        0,
+                        "desc"
+                    ]
+                ]
+            })
+        }
+    );
+});
